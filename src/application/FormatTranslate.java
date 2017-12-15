6c29b5e2f2cdb14ef658d4b6b4fe21710b912c8b
jhu10 <jhu10@nyit.edu>
2017-12-15 12:45:57 +0800

    update

--- src/application/FormatTranslate.java
+++ src/application/FormatTranslate.java
@@ -13,59 +13,97 @@ public class FormatTranslate {
 	        FileWriter tofile = new FileWriter(out);
 	        BufferedWriter bwout = new BufferedWriter(tofile);
 	        	s=br.readLine();	        	
-	          while(s!=null){	        	  
-	        	  if(s.startsWith("MC")) {
-	        		  s="Q1: How many States in the United States? (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"A) 45\r\n" + 
-	        		  		"B) 13\r\n" + 
-	        		  		"C) 50\r\n" + 
-	        		  		"D) 52\r\n";
-	        	  }else if(s.startsWith("MA")) {
-	        		  s="Q2:Which of the following were U.S. Presidents? (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"A) Adams\r\n" + 
-	        		  		"B) Gainey\r\n" + 
-	        		  		"C) Lincoln\r\n" + 
-	        		  		"D) Robinson\r\n";
+	          while(s!=null){
+	        	  String [] str=s.split("\\t");
+	        	  if (s.startsWith("MC")) {
+	        		  str[0]="";
+	        		  str[1]="Q1:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="A) "+str[2];
+	        		  str[3]="";
+	        		  str[4]="B) "+str[4];
+	        		  str[5]="";
+	        		  str[6]="C) "+str[6];
+	        		  str[7]="";
+	        		  str[8]="D) "+str[8];
+	        		  str[9]="";
+	        		  
+	        	  }else if(s.startsWith("MA")&&str[0].contains("T")) {	       
+	        		  str[0]="";
+	        		  str[1]="Q6:"+str[1]+"(10 pts)\r\n"+"\r\n";
+	        		  str[2]="1) "+str[2]+"\t\t\tA)"+str[3];
+	        		  str[3]="";
+	        		  str[4]="2) "+str[4]+"\t\t\tB)"+str[5];
+	        		  str[5]="";
+	        		  str[6]="3) "+str[6]+"\t\t\tC)"+str[7];
+	        		  str[7]="";
 	        	  }else if(s.startsWith("TF")) {
-	        		  s="Q3: The capitol of the United States is New York City (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"True\r\n" + 
-	        		  		"False\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q3:"+str[1]+"(10 pts)"+"\r\n";
+	        		  if(str[2].equals("False")) {
+	        			  str[2]="True"+"\r\n\r\n"+str[2];
+	        		  }else {
+	        			  str[2]=str[2]+"\r\n\r\n"+"False";
+	        		  }
+	        		  
 	        	  }else if(s.startsWith("ESS")) {
-	        		  s="Q4: Write a response of no more than 500 words, using what you have learned in class.(10 pts)\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q4:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="";
+	        		  
 	        	  }else if(s.startsWith("ORD")) {
-	        		  s="Q5: Put the following famous battles in order of occurence.(10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"1. Battle of Haatings\r\n" + 
-	        		  		"2. Battle of Yorktown\r\n" + 
-	        		  		"3. Gettysburg\r\n" + 
-	        		  		"4. Battle of the Somme	\r\n" + 
-	        		  		"5. D-Day\r\n";
-	        	  }else if(s.startsWith("MAT")) {
-	        		  s="Q6: Match the famous battles and the year they were fought.(10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"1. Battle of Hastings			A. 1066\r\n" + 
-	        		  		"2. Gettysburg				B. 1863\r\n" + 
-	        		  		"3. Battle of the Somme		C. 1917\r\n";
-	        	  }else if(s.startsWith("FIB_PLUS	At the CTL [x] and [y] are Bb Support Staff")) {
-	        		  s="Q7: At the CTL ____________ and _____________ are Bb Support Staff (10 pts)\r\n";
-	        	  }else if(s.startsWith("FIB_PLUS	At the CTL [x] Bb Support Staff	x")) {
-	        		  s="Q8: At the CTL ____________ Bb Support Staff (10 pts)\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q5:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="1. "+str[2];
+	        		  str[3]="2. "+str[3];
+	        		  str[4]="3. "+str[4];
+	        		  str[5]="4. "+str[5];
+	        		  str[6]="5. "+str[6];
 	        	  }else if(s.startsWith("FIL")) {
-	        		  s="Q9: What is Ansel Adams most famous photo?\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q9:"+str[1]+"(10 pts)"+"\r\n";
 	        	  }else if(s.startsWith("NUM")) {
-	        		  s="Q10:	e^3 = ?\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q10:"+str[1]+"= ? "+"\r\n";
+	        		  str[2]="";
+	        		  str[3]="";
 	        	  }else if(s.startsWith("SR")) {
-	        		  s="Q11: Describe the flight pattern of flamingo (10pts)\r\n";
-	        	  }
- 		
-    		bwout.write(s);
+	        		  str[0]="";
+	        		  str[1]="Q11:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="";
+	        	  }else if(s.startsWith("FIB_PLUS")&& str[1].contains("[y]")) {
+	        		  str[0]="";
+	        		  str[1]="Q7:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="";
+	        		  str[3]="";
+	        		  str[4]="";
+	        		  str[5]="";
+	        		  str[6]="";
+	        		  str[7]="";
+	        	  }else if(s.startsWith("FIB_PLUS")) {
+	        		  str[0]="";
+	        		  str[1]="Q8:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="";
+	        		  str[3]="";
+	        		  str[4]="";
+	        	  }else if(s.startsWith("MA")) {
+	        		  System.out.println(str.length);
+	        		  str[0]="";
+	        		  str[1]="Q2:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="A) "+str[2];
+	        		  str[3]="";
+	        		  str[4]="B) "+str[4];
+	        		  str[5]="";
+	        		  str[6]="C) "+str[6];
+	        		  str[7]="";
+                      str[8]="D) "+str[8];
+                      str[9]="";
+	        	  }	        	  
+ 		for(int i=0;i<str.length;i++) {			
+    		bwout.write(str[i]);
     		bwout.newLine();
-    		System.out.println(s);
-    		s=br.readLine();
-
+    		   		}
+ 		    s=br.readLine();
+ 		    
 	        }
 	        bwout.close();
 	        br.close();
@@ -85,77 +123,102 @@ public class FormatTranslate {
 	        BufferedWriter bwout = new BufferedWriter(tofile);
 	        	s=br.readLine();	        	
 	          while(s!=null){	        	  
-	        	  if(s.startsWith("MC")) {
-	        		  s="Q1: How many States in the United States? (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"A) 45\r\n" + 
-	        		  		"B) 13\r\n" + 
-	        		  		"C) 50 ----**\r\n" + 
-	        		  		"D) 52\r\n";
-	        	  }else if(s.startsWith("MA")) {
-	        		  s="Q2:Which of the following were U.S. Presidents? (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"A) Adams	----**\r\n" + 
-	        		  		"B) Gainey\r\n" + 
-	        		  		"C) Lincoln----**\r\n" + 
-	        		  		"D) Robinson\r\n";
+	        	  String [] str=s.split("\\t");
+	        	  if (s.startsWith("MC")) {
+	        		  str[0]="";
+	        		  str[1]="Q1:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="A) "+str[2];
+	        		  str[3]="";
+	        		  str[4]="B) "+str[4];
+	        		  str[5]="";
+	        		  str[6]="C) "+str[6]+" ----**";
+	        		  str[7]="";
+	        		  str[8]="D) "+str[8];
+	        		  str[9]="";
+	        		  
+	        	  }else if(s.startsWith("MA")&&str[0].contains("T")) {	       
+	        		  str[0]="";
+	        		  str[1]="Q6:"+str[1]+"(10 pts)\r\n"+"\r\n";
+	        		  str[2]="1) "+str[2]+"\t\t\tA)"+str[3];
+	        		  str[3]="";
+	        		  str[4]="2) "+str[4]+"\t\t\tB)"+str[5];
+	        		  str[5]="";
+	        		  str[6]="3) "+str[6]+"\t\t\tC)"+str[7]+"\r\n";
+	        		  str[7]="1-C, 2-B, 3-A";
 	        	  }else if(s.startsWith("TF")) {
-	        		  s="Q3: The capitol of the United States is New York City (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"True\r\n" + 
-	        		  		"False----**\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q3:"+str[1]+"(10 pts)"+"\r\n";
+	        		  if(str[2].equals("False")) {
+	        			  str[2]="True"+"\r\n\r\n"+str[2]+"----**";
+	        		  }else {
+	        			  str[2]=str[2]+"----**"+"\r\n\r\n"+"False";
+	        		  }
+	        		  
 	        	  }else if(s.startsWith("ESS")) {
-	        		  s="Q4: Write a response of no more than 500 words, using what you have learned in class.(10 pts)\r\n" + 
-	        		  		"bla bla bla bla bla bla bla\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q4:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="bla bla bla bla bla bla bla";
+	        		  
 	        	  }else if(s.startsWith("ORD")) {
-	        		  s="Q5: Put the following famous battles in order of occurence.(10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"1. Battle of Haatings\r\n" + 
-	        		  		"2. Battle of Yorktown\r\n" + 
-	        		  		"3. Gettysburg\r\n" + 
-	        		  		"4. Battle of the Somme	\r\n" + 
-	        		  		"5. D-Day \r\n" + 
-	        		  		"\r\n" + 
-	        		  		"3-4-1-2-5\r\n";
-	        	  }else if(s.startsWith("MAT")) {
-	        		  s="Q6: Match the famous battles and the year they were fought.(10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"1. Battle of Hastings			A. 1066\r\n" + 
-	        		  		"2. Gettysburg				B. 1863\r\n" + 
-	        		  		"3. Battle of the Somme		C. 1917 \r\n" + 
-	        		  		"\r\n" + 
-	        		  		"1-C, 2-B, 3-A\r\n";
-	        	  }else if(s.startsWith("FIB_PLUS	At the CTL [x] and [y] are Bb Support Staff")) {
-	        		  s="Q7: At the CTL ____________ and _____________ are Bb Support Staff (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"answer1, answer2\r\n";
-	        	  }else if(s.startsWith("FIB_PLUS	At the CTL [x] Bb Support Staff	x")) {
-	        		  s="Q8: At the CTL ____________ Bb Support Staff (10 pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"answer\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q5:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="1. "+str[2];
+	        		  str[3]="2. "+str[3];
+	        		  str[4]="3. "+str[4];
+	        		  str[5]="4. "+str[5];
+	        		  str[6]="5. "+str[6]+"\r\n\r\n"+"3-4-1-2-5";
 	        	  }else if(s.startsWith("FIL")) {
-	        		  s="Q9: What is Ansel Adams most famous photo? \r\n" + 
-	        		  		"\r\n" + 
-	        		  		"// this was a file upload questions.\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q9:"+str[1]+"(10 pts)"+"\r\n"+"// this was a file upload questions.";
 	        	  }else if(s.startsWith("NUM")) {
-	        		  s="Q10:	e^3 = ?\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"	20.09\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q10:"+str[1]+"= ? "+"\r\n";
+	        		  str[2]=str[2];
+	        		  str[3]="";
 	        	  }else if(s.startsWith("SR")) {
-	        		  s="Q11: Describe the flight pattern of flamingo (10pts)\r\n" + 
-	        		  		"\r\n" + 
-	        		  		"	They fly in a V formation\r\n";
+	        		  str[0]="";
+	        		  str[1]="Q11:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]=str[2];
+	        	  }else if(s.startsWith("FIB_PLUS")&& str[1].contains("[y]")) {
+	        		  str[0]="";
+	        		  str[1]="Q7:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="answer1\r\n";
+	        		  str[3]="";
+	        		  str[4]="answer2";
+	        		  str[5]="";
+	        		  str[6]="";
+	        		  str[7]="";
+	        	  }else if(s.startsWith("FIB_PLUS")) {
+	        		  str[0]="";
+	        		  str[1]="Q8:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="anwser";
+	        		  str[3]="";
+	        		  str[4]="";
+	        	  }else if(s.startsWith("MA")) {
+	        		  System.out.println(str.length);
+	        		  str[0]="";
+	        		  str[1]="Q2:"+str[1]+"(10 pts)"+"\r\n";
+	        		  str[2]="A) "+str[2]+	"----**";
+	        		  str[3]="";
+	        		  str[4]="B) "+str[4];
+	        		  str[5]="";
+	        		  str[6]="C) "+str[6]+"----**";
+	        		  str[7]="";
+                      str[8]="D) "+str[8];
+                      str[9]="";
 	        	  }
- 		
-    		bwout.write(s);
-    		bwout.newLine();
-    		System.out.println(s);
-    		s=br.readLine();
-
-	        }
-	        bwout.close();
-	        br.close();
-	        tofile.close();
+	        	  for(int i=0;i<str.length;i++) {
+	       			
+	          		bwout.write(str[i]);
+	          		bwout.newLine();
+	          		System.out.println(s);
+	          		   		}
+	       		    s=br.readLine();
+	       		    
+	      	        }
+	      	        bwout.close();
+	      	        br.close();
+	      	        tofile.close();
     		} catch (IOException e) {
     		// TODO Auto-generated catch block
     		e.printStackTrace();
